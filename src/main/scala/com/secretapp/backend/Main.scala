13909bfd82bdd5220d65d74060cec2826ec15eab
Andrey Kouznetsov <fear@loathing.in>
2014-07-01 02:51:04 +0600

    Turned app into Bootable Akka Microkernel app

--- src/main/scala/com/secretapp/backend/Main.scala
+++ src/main/scala/com/secretapp/backend/Main.scala
@@ -3,15 +3,25 @@ package com.secretapp.backend
 import java.net.InetSocketAddress
 import akka.actor.{ ActorSystem, Props }
 import akka.io.{ IO, Tcp }
+import akka.kernel.Bootable
 import Tcp._
 import api.Server
 
-object Main extends App {
+object ApiKernel extends Bootable {
 
   implicit val system = ActorSystem("secret-api-server")
 
-  implicit val service = system.actorOf(Props[Server], "api-service")
+  def startup = {
+    implicit val service = system.actorOf(Props[Server], "api-service")
+    IO(Tcp) ! Bind(service, new InetSocketAddress("0.0.0.0", 8080))
+  }
 
-  IO(Tcp) ! Bind(service, new InetSocketAddress("0.0.0.0", 8080))
+  def shutdown = {
+    system.shutdown()
+  }
+}
 
+// Just for repl and testing purposes
+object Main extends App {
+  ApiKernel.startup()
 }
