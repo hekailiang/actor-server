9e327e5f98d2b0a9163204f4922e98ccc7610dcb
Timothy Klim <klimtimothy@gmail.com>
2014-06-30 20:57:05 +0400

    mock Random

--- src/test/scala/com/secretapp/backed/api/ApiHandlerSpec.scala
+++ src/test/scala/com/secretapp/backed/api/ApiHandlerSpec.scala
@@ -7,9 +7,10 @@ import akka.actor.{ ActorSystem, Props }
 import akka.io.Tcp._
 import akka.util.ByteString
 import scodec.bits._
+import scala.util.Random
 import com.secretapp.backend.api.ApiHandler
 import com.secretapp.backend.protocol.Package
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.{ ConcurrentHashMap, ConcurrentSkipListSet }
 
 class ApiHandlerSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike
   with Matchers with BeforeAndAfterAll {
@@ -22,16 +23,20 @@ class ApiHandlerSpec(_system: ActorSystem) extends TestKit(_system) with Implici
     TestKit.shutdownActorSystem(system)
   }
 
-  val authTable = new ConcurrentHashMap[Long, Long]()
+  val authTable = new ConcurrentHashMap[Long, ConcurrentSkipListSet[Long]]()
   val probe = TestProbe()
-  val apiActor = system.actorOf(Props(classOf[ApiHandler], authTable))
+  val apiActor = system.actorOf(Props(new ApiHandler(authTable) {
+    override lazy val rand = new Random() {
+      override def nextLong() = 12345L
+    }
+  }))
 
 
   "actor" must {
 
     "reply pong to ping" in {
       val req = Package.encode(0L, 0L, 1L, RequestAuthId()).toOption.get.toByteBuffer
-      val res = Package.encode(0L, 0L, 1L, ResponseAuthId(1L)).toOption.get.toByteBuffer
+      val res = Package.encode(0L, 0L, 1L, ResponseAuthId(12345L)).toOption.get.toByteBuffer
       probe.send(apiActor, Received(ByteString(req)))
       probe.expectMsg(Write(ByteString(res)))
     }
