8ed57eb3232d981e08361c68450424a428ef3dac
Andrey Kouznetsov <fear@loathing.in>
2014-07-12 20:39:28 +0600

    configurable listen port

--- src/main/scala/com/secretapp/backend/ApiKernel.scala
+++ src/main/scala/com/secretapp/backend/ApiKernel.scala
@@ -7,15 +7,17 @@ import akka.kernel.Bootable
 import Tcp._
 import api.Server
 import com.secretapp.backend.persist.DBConnector
+import com.typesafe.config._
 
 class ApiKernel extends Bootable {
+  val serverConfig = ConfigFactory.load().getConfig("secret.server")
 
   implicit val system = ActorSystem("secret-api-server")
 
   def startup = {
     val session = DBConnector.session
     implicit val service = system.actorOf(Props(new Server(session)), "api-service")
-    IO(Tcp) ! Bind(service, new InetSocketAddress("0.0.0.0", 8080))
+    IO(Tcp) ! Bind(service, new InetSocketAddress("0.0.0.0", serverConfig.getInt("port")))
   }
 
   def shutdown = {
