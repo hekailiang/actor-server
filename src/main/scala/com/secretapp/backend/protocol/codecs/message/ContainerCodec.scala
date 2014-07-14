8bfd548c6c12471e2c72db83f0a38723b2a3bf37
Timothy Klim <klimtimothy@gmail.com>
2014-07-14 04:33:57 +0400

    wip(protocol): decode transport containers

--- src/main/scala/com/secretapp/backend/protocol/codecs/message/ContainerCodec.scala
+++ src/main/scala/com/secretapp/backend/protocol/codecs/message/ContainerCodec.scala
@@ -18,10 +18,31 @@ object ContainerCodec extends Codec[Container] {
     foldEithers(encodeMessages)(BitVector.empty)(_ ++ _)
   }
 
-  def decode(buf : BitVector) = ???
+  def decode(buf : BitVector) = {
+    @tailrec
+    def messages(buf : BitVector)(acc : Seq[MessageBox]) : String \/ Seq[MessageBox] = {
+      MessageBoxCodec.decode(buf) match {
+        case \/-((buf, m)) => m match {
+          case MessageBox(_, Container(_)) => "container can't be nested".left
+          case m : MessageBox =>
+            val res = acc :+ m
+            if (buf.isEmpty) {
+              res.right
+            } else {
+              messages(buf)(res)
+            }
+        }
+        case l@(-\/(_)) => l
+      }
+    }
+    messages(buf)(Seq()) match {
+      case \/-(msgs) => (BitVector.empty, Container(msgs)).right
+      case l@(-\/(_)) => l
+    }
+  }
 
   @tailrec
-  private def foldEithers[A, B](items : Seq[A \/ B])(acc : B)(f : (B, B) => B) : A \/ B = items match {
+  private def foldEithers[A, B](seq : Seq[A \/ B])(acc : B)(f : (B, B) => B) : A \/ B = seq match {
     case x :: xs => x match {
       case \/-(r) => foldEithers(xs)(f(acc, r))(f)
       case l@(-\/(_)) => l
--- src/test/scala/com/secretapp/backend/protocol/codecs/message/ContainerCodecSpec.scala
+++ src/test/scala/com/secretapp/backend/protocol/codecs/message/ContainerCodecSpec.scala
@@ -9,7 +9,7 @@ import Scalaz._
 import org.specs2.mutable._
 
 class ContainerCodecSpec extends Specification {
-  "container" should {
+  "container encode" should {
     "pack messages" in {
       val res = hex"000000000000000109010000000000000005000000000000000109010000000000000005000000000000000109010000000000000005".bits
       val ping = MessageBox(1L, Ping(5L))
@@ -23,6 +23,21 @@ class ContainerCodecSpec extends Specification {
       val nested = MessageBox(1L, Container(Seq(ping)))
       val c = Container(Seq(ping, ping, nested))
       ContainerCodec.encode(c) must beEqualTo(res.left)
+    }
+  }
+
+  "container decode" should {
+    "decode binary to original container" in {
+      val stream = hex"000000000000000109010000000000000005000000000000000109010000000000000005000000000000000109010000000000000005".bits
+      val ping = MessageBox(1L, Ping(5L))
+      val res = Container(Seq(ping, ping, ping))
+      ContainerCodec.decode(stream) must beEqualTo((BitVector.empty, res).right)
+    }
+
+    "raise error if it have nested container detected" in {
+      val stream = hex"0000000000000001090100000000000000050000000000000001090100000000000000050000000000000001130a000000000000000109010000000000000005".bits
+      val res = "container can't be nested"
+      ContainerCodec.decode(stream) must beEqualTo(res.left)
     }
   }
 }
