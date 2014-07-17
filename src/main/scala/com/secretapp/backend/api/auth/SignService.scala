1957fd909fac46af404447d6e72bf1e94da76988
Timothy Klim <klimtimothy@gmail.com>
2014-07-17 16:30:21 +0400

    wip(auth): remove spaghetti callbacks

--- src/main/scala/com/secretapp/backend/api/auth/SignService.scala
+++ src/main/scala/com/secretapp/backend/api/auth/SignService.scala
@@ -3,6 +3,7 @@ package com.secretapp.backend.api.auth
 import akka.actor._
 import scala.collection.immutable.Seq
 import scala.util.{ Random, Try, Success, Failure }
+import scala.concurrent.Future
 import com.typesafe.config.ConfigFactory
 import com.datastax.driver.core.{ Session => CSession }
 import com.secretapp.backend.api.PackageCommon
@@ -24,48 +25,36 @@ trait SignService extends PackageCommon { self: Actor =>
 
   import context._
 
+  type RpcResult = HandleResult[Error, RpcResponseMessage]
+
   def handleRpcAuth(p: Package, messageId: Long): PartialFunction[RpcRequestMessage, Any] = {
     case r: RequestAuthCode => (handleRequestAuthCode(p, messageId) _).tupled(RequestAuthCode.unapply(r).get)
     case r: RequestSignIn => (handleRequestSignIn(p, messageId) _).tupled(RequestSignIn.unapply(r).get)
     case r: RequestSignUp => (handleRequestSignUp(p, messageId) _).tupled(RequestSignUp.unapply(r).get)
   }
 
-  def handleRequestAuthCode(p: Package, messageId: Long)(phoneNumber: Long, appId: Int, apiKey: String): Unit = {
+  def handleRequestAuthCode(p: Package, messageId: Long)(phoneNumber: Long, appId: Int, apiKey: String): RpcResult = {
     val smsCode = rand.nextLong().toString.drop(1).take(6)
     val smsHash = rand.nextLong().toString
 
     val serverConfig = ConfigFactory.load()
     val clickatell = new ClickatellSMSEngine(serverConfig) // TODO: use singleton for share config env
-    // TODO: validate number
-
-    // #400: PHONE_NUMBER_INVALID - неверный номер телефона. Отображается пользователю.
-    // TODO: Service unreachable?
 
     val f = for {
       _ <- clickatell.send(phoneNumber.toString, s"Your secret app activation code: $smsCode") // TODO: singleton for model
       phoneR <- PhoneRecord.getEntity(phoneNumber)
       _ <- AuthSmsCodeRecord.insertEntity(AuthSmsCode(phoneNumber, smsHash, smsCode))
     } yield ResponseAuthCode(smsHash, phoneR.isDefined)
-    // TODO: DRY
-    f.onComplete  {
-      case Success(res) =>
-        val reply = p.replyWith(messageId, RpcResponseBox(messageId, Ok(res))).right
-        handleActor ! PackageToSend(reply)
-      case Failure(e) =>
-        val error = Error(400, "PHONE_NUMBER_INVALID", e.getMessage, true) // TODO
-        val reply = p.replyWith(messageId, RpcResponseBox(messageId, error)).right
-        handleActor ! PackageToSend(reply)
+    f.flatMap(r => Future.successful(r.right)).recover {
+      case e: Throwable =>
+        Error(400, "PHONE_NUMBER_INVALID", e.getMessage, true).left
     }
   }
 
-  private def validateSignParams(phoneNumber: Long,
-                                 smsHash: String,
-                                 smsCode: String)(smsCodeR: AuthSmsCode): Error \/ Unit = {
+  private def validateSignParams(smsHash: String, smsCode: String)(smsCodeR: AuthSmsCode): Error \/ Unit = {
     smsCodeR match {
       case s if s.smsHash != smsHash =>
         Error(400, "PHONE_CODE_EXPIRED", "", true).left
-      case s if s.smsCode.isEmpty =>
-        Error(400, "PHONE_CODE_EMPTY", "", true).left
       case s if s.smsCode != smsCode =>
         Error(400, "PHONE_CODE_INVALID", "", true).left
       case _ => ().right
@@ -75,28 +64,20 @@ trait SignService extends PackageCommon { self: Actor =>
   def handleRequestSignIn(p: Package, messageId: Long)(phoneNumber: Long,
                                                        smsHash: String,
                                                        smsCode: String,
-                                                       publicKey: BitVector): Unit =
-  {
-    PhoneRecord.getEntity(phoneNumber).onComplete {
-      case Success(Some(phoneR)) => // TODO: remove Success(Some( for pattern matching
-        AuthSmsCodeRecord.getEntity(phoneNumber).onComplete {
-          case Success(Some(smsCodeR)) =>
-            validateSignParams(phoneNumber, smsHash, smsCode)(smsCodeR).rightMap { _ =>
-              phoneR.user.onComplete {
-                case Success(Some(user)) =>
-                  val sUser = StructUser(phoneR.userId, user.accessHash, user.firstName, user.lastName, user.sex, Seq(1L))
-                  Ok(ResponseAuth(123L, sUser))
-                case _ =>
-//                  Internal error
-              }
-            }
-          case _ =>
-            val error = Error(400, "PHONE_CODE_EMPTY", "", true) // TODO
-            sendPackage(p, messageId)(RpcResponseBox(messageId, error))
+                                                       publicKey: BitVector): RpcResult = {
+    if (smsCode.isEmpty) {
+      Future.successful(Error(400, "PHONE_CODE_EMPTY", "", true).left)
+    } else {
+      for {
+        phoneR <- futOptHandle(PhoneRecord.getEntity(phoneNumber), Error(400, "PHONE_NUMBER_UNOCCUPIED", "", true))
+        user <- futOptHandle(phoneR.user, Error(400, "INTERNAL_ERROR", "", true))
+        smsCodeR <- futOptHandle(AuthSmsCodeRecord.getEntity(phoneNumber), Error(400, "PHONE_CODE_EXPIRED", "", true))
+      } yield {
+        validateSignParams(smsHash, smsCode)(smsCodeR).rightMap { _ =>
+          val sUser = StructUser(phoneR.userId, user.accessHash, user.firstName, user.lastName, user.sex, Seq(1L))
+          ResponseAuth(123L, sUser)
         }
-      case _ =>
-        val error = Error(400, "PHONE_NUMBER_UNOCCUPIED", "", true) // TODO
-        sendPackage(p, messageId)(RpcResponseBox(messageId, error))
+      }
     }
   }
 
@@ -105,12 +86,16 @@ trait SignService extends PackageCommon { self: Actor =>
                                                        smsCode: String,
                                                        firstName: String,
                                                        lastName: Option[String],
-                                                       publicKey: BitVector): Unit = {
-    ???
+                                                       publicKey: BitVector): RpcResult = {
+    if (smsCode.isEmpty) {
+      Future.successful(Error(400, "PHONE_CODE_EMPTY", "", true).left)
+    } else {
+      ???
+    }
   }
 
-  private def sendPackage(p: Package, messageId: Long)(tm: TransportMessage): Unit = {
-    val reply = p.replyWith(messageId, tm).right
-    handleActor ! PackageToSend(reply)
-  }
+//  private def sendPackage(p: Package, messageId: Long)(tm: TransportMessage): Unit = {
+//    val reply = p.replyWith(messageId, tm).right
+//    handleActor ! PackageToSend(reply)
+//  }
 }
