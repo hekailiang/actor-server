2450d5f5ef4c152efab874032ca405adc81ef43b
Andrey Kouznetsov <fear@loathing.in>
2014-07-25 22:19:19 +0400

    chore(*): removed unused extra actor

--- src/main/scala/com/secretapp/backend/api/ExtraActorsInitializer.scala
+++ src/main/scala/com/secretapp/backend/api/ExtraActorsInitializer.scala
@@ -4,7 +4,5 @@ import akka.actor._
 
 trait ExtraActorsInitializer {
   self: Actor with ActorLogging =>
-    val ref = context.system.actorOf(Props(new CounterActor("message-counter")), "message-counter")
-    log.info("MessageCounter started {}", ref.path.toStringWithoutAddress)
     context.system.actorOf(Props(new CounterActor("user-counter")), "user-counter")
 }
