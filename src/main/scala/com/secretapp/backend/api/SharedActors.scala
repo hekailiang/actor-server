ac382d625bdba4baf56e43d17c29a1277c50d111
Andrey Kouznetsov <fear@loathing.in>
2014-08-01 00:49:06 +0400

    fix(messaging): wrong SharedActors behaviour which created an actor each time it tries to resolve shared actor

--- src/main/scala/com/secretapp/backend/api/SharedActors.scala
+++ src/main/scala/com/secretapp/backend/api/SharedActors.scala
@@ -13,19 +13,18 @@ object SharedActors {
     * Resolves ActorRef by path and creates a new actor if is is absent
     */
   def lookup(path: String)(f: => ActorRef)(implicit system: ActorSystem, timeout: Timeout): Future[ActorRef] = {
-    val spath = s"shared~$path"
-    val selection = system.actorSelection(spath)
+    val selection = system.actorSelection(path)
     import system.dispatcher
 
     selection.resolveOne recover {
       case e: ActorNotFound =>
-        log.info(s"Creating $spath")
+        log.info(s"Creating $path")
         val ref = f
-        log.info(s"Created $spath")
+        log.info(s"Created $path")
         ref
     } andThen {
       case Failure(e) =>
-        log.error(s"Cannot resolve $spath {}", e)
+        log.error(s"Cannot resolve $path {}", e)
     }
   }
 }
