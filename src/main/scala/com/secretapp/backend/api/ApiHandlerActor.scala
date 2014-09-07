138c723246dcca49e6bbf7674981bdd77adfc79a
Andrey Kouznetsov <fear@loathing.in>
2014-09-07 19:26:29 +0400

    chore(transport): write acks

--- src/main/scala/com/secretapp/backend/api/ApiHandlerActor.scala
+++ src/main/scala/com/secretapp/backend/api/ApiHandlerActor.scala
@@ -2,7 +2,6 @@ package com.secretapp.backend.api
 
 import akka.actor.{ Actor, ActorRef, ActorLogging }
 import akka.util.ByteString
-import akka.io.Tcp._
 import com.secretapp.backend.data.transport.MessageBox
 import scodec.bits._
 import com.secretapp.backend.data.transport.Package
@@ -19,6 +18,10 @@ import com.datastax.driver.core.{ Session => CSession }
 // TODO: replace connection: ActorRef hack with real sender (or forget it?)
 class ApiHandlerActor(connection: ActorRef, val clusterProxies: ClusterProxies)(implicit val session: CSession) extends Actor with ActorLogging
     with WrappedPackageService with PackageService {
+  import akka.io.Tcp._
+  case class Ack(stamp: Int) extends Event
+  var writeStamp = 0
+
   val handleActor = self
 
   context watch connection
@@ -66,10 +69,14 @@ class ApiHandlerActor(connection: ActorRef, val clusterProxies: ClusterProxies)(
     case Closed =>
       log.info(s"Connection closed by listener")
       context stop self
+
+    case Ack(stamp) =>
+      log.info("Ack ${stamp}")
   }
 
   def write(connection: ActorRef, byteString: ByteString): Unit = {
     log.debug(s"Sending ${connection} ${byteString}")
-    connection ! Write(byteString)
+    writeStamp += 1
+    connection ! Write(byteString, Ack(writeStamp))
   }
 }
