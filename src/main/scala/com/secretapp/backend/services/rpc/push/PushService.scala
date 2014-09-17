a83037b937e3a1bc06f2f0cfc0e4612ca00929ef
Paul L <paul-mail@yandex.ru>
2014-09-17 14:34:20 +0700

    feat(push): PushService handler added to ApiBrokerService handling routine

--- src/main/scala/com/secretapp/backend/services/rpc/push/PushService.scala
+++ src/main/scala/com/secretapp/backend/services/rpc/push/PushService.scala
@@ -6,14 +6,16 @@ import com.secretapp.backend.api.ApiBrokerService
 import com.secretapp.backend.api.rpc.RpcProtocol
 import com.secretapp.backend.data.message.rpc.{RpcResponse, RpcRequestMessage}
 import com.secretapp.backend.data.message.rpc.push.{RequestUnregisterPush, RequestRegisterGooglePush}
-import com.secretapp.backend.data.transport.Package
+import scalaz._
+
+import scala.concurrent.Future
 
 trait PushService {
   this: ApiBrokerService =>
 
   lazy val handler = context.actorOf(Props(new Handler(currentUser.get)), "pushes")
 
-  def handleRpcPush(p: Package, messageId: Long): PartialFunction[RpcRequestMessage, Any] = {
+  val handleRpcPush: PartialFunction[RpcRequestMessage, \/[Throwable, Future[RpcResponse]]] = {
     case r @ (_: RequestRegisterGooglePush | _: RequestUnregisterPush) =>
       authorizedRequest {
         (handler ? RpcProtocol.Request(r)).mapTo[RpcResponse]
