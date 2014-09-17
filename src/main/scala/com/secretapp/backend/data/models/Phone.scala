81244d96ecca8ece4278d08bdb8484f6e557de8a
Paul L <paul-mail@yandex.ru>
2014-09-17 11:55:37 +0700

    feat(push): GooglePushCredentials model

--- src/main/scala/com/secretapp/backend/data/models/Phone.scala
+++ src/main/scala/com/secretapp/backend/data/models/Phone.scala
@@ -1,9 +1,6 @@
 package com.secretapp.backend.data.models
 
-import scala.concurrent.Future
 import scala.collection.immutable
-import com.datastax.driver.core.Session
-import com.secretapp.backend.persist._
 import com.secretapp.backend.data.types._
 
 case class Phone(number: Long, userId: Int, userAccessSalt: String, userName: String,
