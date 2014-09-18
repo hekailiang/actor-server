5f20b12ef0e34b157f0f5ec835cec044cc9dae3d
Paul L <paul-mail@yandex.ru>
2014-09-18 16:55:50 +0700

    chore(specs): PushServiceSpec refactored

--- src/test/scala/com/secretapp/backend/services/rpc/push/PushServiceSpec.scala
+++ src/test/scala/com/secretapp/backend/services/rpc/push/PushServiceSpec.scala
@@ -1,49 +1,43 @@
 package com.secretapp.backend.services.rpc.push
 
-import com.secretapp.backend.data.message.rpc.auth.ResponseAuth
 import com.secretapp.backend.data.models.GooglePushCredentials
 import com.secretapp.backend.persist.GooglePushCredentialsRecord
 import com.secretapp.backend.services.rpc.RpcSpec
 import com.secretapp.backend.data.message.rpc.push._
 import com.secretapp.backend.data.message.rpc.ResponseVoid
 import com.newzly.util.testing.AsyncAssertionsHelper._
+import org.specs2.specification.BeforeExample
 import scalaz._
 import Scalaz._
 
-class PushServiceSpec extends RpcSpec {
+class PushServiceSpec extends RpcSpec with BeforeExample {
   import system.dispatcher
 
   "push service" should {
 
     "respond to `RequestRegisterGooglePush` with `ResponseVoid`" in {
-      implicit val scope = TestScope()
-
       registerShouldBeOk
     }
 
     "store credentials passed on `RequestRegisterGooglePush` request" in {
-      implicit val scope = TestScope()
-
       registerShouldBeOk
-
       storedCreds should_== creds.some
     }
 
     "respond to `RequestUnregisterPush` with OK even if device is not registered first" in {
-      implicit val scope = TestScope()
-
       unregisterShouldBeOk
     }
 
     "remove credentials on `RequestUnregisterPush`" in {
-      implicit val scope = TestScope()
-
       registerShouldBeOk
       unregisterShouldBeOk
-
       storedCreds should_== None
     }
   }
+
+  private implicit var scope: TestScope = _
+
+  override def before = scope = TestScope()
 
   private def creds(implicit scope: TestScope) = GooglePushCredentials(scope.user.uid, scope.user.authId, 42, "reg id")
 
