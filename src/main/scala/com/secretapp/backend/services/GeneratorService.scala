228b86294ff3e78033baf003f35d24faccfbfaa6
Andrey Kouznetsov <fear@loathing.in>
2014-09-24 02:07:06 +0400

    feat(sign): no sms codes starting with 0

--- src/main/scala/com/secretapp/backend/services/GeneratorService.scala
+++ src/main/scala/com/secretapp/backend/services/GeneratorService.scala
@@ -5,7 +5,7 @@ import com.secretapp.backend.services.common.RandomService
 trait GeneratorService extends RandomService {
   def genNewAuthId = rand.nextLong
 
-  def genSmsCode = rand.nextLong().toString.drop(1).take(6)
+  def genSmsCode = rand.nextLong().toString.take(6)
 
   def genSmsHash = rand.nextLong().toString
 
