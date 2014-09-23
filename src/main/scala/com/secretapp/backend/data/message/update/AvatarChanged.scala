2f0755dcb6f2cff3ee9ca299dedf493e9194a828
Paul L <paul-mail@yandex.ru>
2014-09-23 18:15:59 +0700

    chore(contact): ContactRegistered update

--- src/main/scala/com/secretapp/backend/data/message/update/AvatarChanged.scala
+++ src/main/scala/com/secretapp/backend/data/message/update/AvatarChanged.scala
@@ -3,11 +3,11 @@ package com.secretapp.backend.data.message.update
 import com.secretapp.backend.data.message.struct.Avatar
 
 case class AvatarChanged(uid: Int, avatar: Option[Avatar]) extends CommonUpdateMessage {
-  val commonUpdateType = AvatarChanged.commonUpdateType
+  override val commonUpdateType = AvatarChanged.commonUpdateType
 
-  def userIds: Set[Int] = Set(uid)
+  override def userIds: Set[Int] = Set(uid)
 }
 
 object AvatarChanged extends CommonUpdateMessageObject {
-  val commonUpdateType = 0x10
+  override val commonUpdateType = 0x10
 }
