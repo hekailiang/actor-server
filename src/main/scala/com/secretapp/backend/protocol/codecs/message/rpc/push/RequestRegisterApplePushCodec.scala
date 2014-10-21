2dcc73645cea67121b71dccc34bd0bd12ea13b2a
Andrey Kouznetsov <fear@loathing.in>
2014-10-21 16:33:12 +0400

    fix(push): fixed RequestRegisterGooglePush and RequestRegisterApplePush

--- src/main/scala/com/secretapp/backend/protocol/codecs/message/rpc/push/RequestRegisterApplePushCodec.scala
+++ src/main/scala/com/secretapp/backend/protocol/codecs/message/rpc/push/RequestRegisterApplePushCodec.scala
@@ -2,15 +2,23 @@ package com.secretapp.backend.protocol.codecs.message.rpc.push
 
 import com.secretapp.backend.data.message.rpc.push.RequestRegisterApplePush
 import com.secretapp.backend.protocol.codecs._
+import com.secretapp.backend.protocol.codecs.utils.protobuf._
+import im.actor.messenger.{ api => protobuf }
+import scala.util.Success
 import scodec.Codec
 import scodec.bits._
 import scodec.codecs._
 
-object RequestRegisterApplePushCodec extends Codec[RequestRegisterApplePush] {
+object RequestRegisterApplePushCodec extends Codec[RequestRegisterApplePush] with utils.ProtobufCodec {
+  def encode(r: RequestRegisterApplePush) = {
+    val boxed = protobuf.RequestRegisterApplePush(r.apnsKey, r.token)
+    encodeToBitVector(boxed)
+  }
 
-  private val codec = (int32 :: protoString).as[RequestRegisterApplePush]
-
-  override def encode(r: RequestRegisterApplePush) = codec.encode(r)
-
-  override def decode(buf: BitVector) = codec.decode(buf)
+  def decode(buf: BitVector) = {
+    decodeProtobuf(protobuf.RequestRegisterApplePush.parseFrom(buf.toByteArray)) {
+      case Success(protobuf.RequestRegisterApplePush(apnsKey, token)) =>
+        RequestRegisterApplePush(apnsKey, token)
+    }
+  }
 }
--- src/main/scala/com/secretapp/backend/protocol/codecs/message/rpc/push/RequestRegisterGooglePushCodec.scala
+++ src/main/scala/com/secretapp/backend/protocol/codecs/message/rpc/push/RequestRegisterGooglePushCodec.scala
@@ -2,15 +2,23 @@ package com.secretapp.backend.protocol.codecs.message.rpc.push
 
 import com.secretapp.backend.data.message.rpc.push.RequestRegisterGooglePush
 import com.secretapp.backend.protocol.codecs._
+import com.secretapp.backend.protocol.codecs.utils.protobuf._
+import im.actor.messenger.{ api => protobuf }
+import scala.util.Success
 import scodec.Codec
 import scodec.bits._
 import scodec.codecs._
 
-object RequestRegisterGooglePushCodec extends Codec[RequestRegisterGooglePush] {
+object RequestRegisterGooglePushCodec extends Codec[RequestRegisterGooglePush] with utils.ProtobufCodec {
+  def encode(r: RequestRegisterGooglePush) = {
+    val boxed = protobuf.RequestRegisterGooglePush(r.projectId, r.token)
+    encodeToBitVector(boxed)
+  }
 
-  private val codec = (int64 :: protoString).as[RequestRegisterGooglePush]
-
-  override def encode(r: RequestRegisterGooglePush) = codec.encode(r)
-
-  override def decode(buf: BitVector) = codec.decode(buf)
+  def decode(buf: BitVector) = {
+    decodeProtobuf(protobuf.RequestRegisterGooglePush.parseFrom(buf.toByteArray)) {
+      case Success(protobuf.RequestRegisterGooglePush(projectId, token)) =>
+        RequestRegisterGooglePush(projectId, token)
+    }
+  }
 }
