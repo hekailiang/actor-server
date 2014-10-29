b161bc1d0e6fc0fa66fe9187f003f78009326574
Andrey Kouznetsov <fear@loathing.in>
2014-10-29 16:12:30 +0300

    feat(gcm): use collapse_key

--- src/main/scala/com/secretapp/backend/api/GooglePush.scala
+++ src/main/scala/com/secretapp/backend/api/GooglePush.scala
@@ -23,35 +23,36 @@ trait GooglePush {
       .addHeader("Authorization", s"key=$token")
       .setContentType("application/json", "UTF-8")
 
-  private def payload(regId: String, seq: Int) =
+  private def payload(regId: String, collapseKey: String, seq: Int) =
     s"""
        |{
        |  "registration_ids": ["$regId"],
+       |  "collapse_key": "$collapseKey",
        |  "dry_run": $dryRun,
        |  "data": {
        |    "seq": $seq
        |  }
        |}""".stripMargin
 
-  private def request(regId: String, seq: Int) = basicRequest.setBody(payload(regId, seq))
+  private def request(regId: String, collapseKey: String, seq: Int) = basicRequest.setBody(payload(regId, collapseKey, seq))
 
 
-  private def sendGooglePush(regId: String, seq: Int): Future[Unit] =
-    Http(request(regId, seq) OK as.String).either map {
+  private def sendGooglePush(regId: String, collapseKey: String, seq: Int): Future[Unit] =
+    Http(request(regId, collapseKey, seq) OK as.String).either map {
       case Right(_) => log.debug("GCM push succeed")
       case Left(e)  => log.error(s"GCM push failed: ${e.getMessage}")
     }
 
-  private def deliverGooglePush(optCreds: Option[GooglePushCredentials], seq: Int)
+  private def deliverGooglePush(optCreds: Option[GooglePushCredentials], authId: Long, seq: Int)
                                (implicit s: CSession): Future[Unit] =
     optCreds some { c =>
-      sendGooglePush(c.regId, seq)
+      sendGooglePush(c.regId, authId.toString, seq)
     } none Future.successful()
 
   def deliverGooglePush(authId: Long, seq: Int)
                        (implicit s: CSession): Future[Unit] =
     GooglePushCredentialsRecord.get(authId) flatMap {
-      deliverGooglePush(_, seq)
+      deliverGooglePush(_, authId, seq)
     }
 
 }
