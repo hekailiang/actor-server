b1251033193235339dc749e37898c92dcd6f6330
Timothy Klim <klimtimothy@gmail.com>
2015-02-01 21:54:06 +0700

    fix(rest-api): user default avatar

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 set -e
 export JAVA_OPTS="-Xms512M -Xmx2048M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512M"
+cp -a ./actor-restapi/public ./src/main/resources/
 sbt clean dist
+rm -rf ./src/main/resources/public
 ansible-playbook -vvv -i ./ansible/hosts ./ansible/deploy.yml
-# rsync -avzc --delete -r target/dist/ --exclude bin/start --exclude config/application.conf --exclude apns ec2-user@172.31.14.18:~/dist
-# ssh -v ec2-user@172.31.14.18 'cp ~/application.conf ~/dist/config;cp -a ~/apns ~/dist/;cp ~/start ~/dist/bin/;chmod +x ~/dist/bin/start'
-# ssh -v ec2-user@172.31.14.18 'pkill java & nohup ~/dist/bin/start >> ~/server.log 2>>~/server-err.log &'
 echo "Deployed successfully."
