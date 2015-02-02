ee9d7be77bc299c9a3ecba8206c42c6da12b174d
Timothy Klim <klimtimothy@gmail.com>
2015-02-02 12:12:43 +0700

    fix(*): deploy issues

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 set -e
 export JAVA_OPTS="-Xms512M -Xmx2048M -Xss1M -XX:+CMSClassUnloadingEnabled"
 cp -a ./actor-restapi/public ./src/main/resources/
-# sbt clean dist
+sbt clean dist
 rm -rf ./src/main/resources/public
 # ansible-galaxy install -r ./ansible/requirements.yml
-echo "for install requirements: ansible-galaxy install -r ./ansible/requirements.yml"
+echo "For install requirements: ansible-galaxy install -r ./ansible/requirements.yml"
 ansible-playbook -vvv -i ./ansible/hosts ./ansible/deploy.yml
 echo "Deployed successfully."
