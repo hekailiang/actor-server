457995b1ab3ba61a84b0a4b3d5dec7baa206fc6d
Timothy Klim <klimtimothy@gmail.com>
2015-02-02 12:05:06 +0700

    feat(*): deploy with ansible + supervisord for manage running state

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
-export JAVA_OPTS="-Xms512M -Xmx2048M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=512M"
+export JAVA_OPTS="-Xms512M -Xmx2048M -Xss1M -XX:+CMSClassUnloadingEnabled"
 cp -a ./actor-restapi/public ./src/main/resources/
-sbt clean dist
+# sbt clean dist
 rm -rf ./src/main/resources/public
+# ansible-galaxy install -r ./ansible/requirements.yml
+echo "for install requirements: ansible-galaxy install -r ./ansible/requirements.yml"
 ansible-playbook -vvv -i ./ansible/hosts ./ansible/deploy.yml
 echo "Deployed successfully."
