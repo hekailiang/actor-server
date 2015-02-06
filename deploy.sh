3d790e7523055c6835a176ebe0022d6ff7b583f8
Timothy Klim <klimtimothy@gmail.com>
2015-02-06 21:00:54 +0300

    feat(deploy): switch to monit

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 export JAVA_OPTS="-Xms512M -Xmx2048M -Xss1M -XX:+CMSClassUnloadingEnabled"
-cp -a ./actor-restapi/public ./src/main/resources/
-sbt clean dist
-rm -rf ./src/main/resources/public
+#sbt clean dist
 # ansible-galaxy install -r ./ansible/requirements.yml
-echo "\n!!!\nFor install requirements: ansible-galaxy install -r ./ansible/requirements.yml\n!!!\n"
+echo "!!! For install requirements: ansible-galaxy install -r ./ansible/requirements.yml !!!"
 ansible-playbook -vvv -i ./ansible/hosts ./ansible/deploy.yml
 echo "Deployed successfully."
