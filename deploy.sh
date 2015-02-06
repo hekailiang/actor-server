bf2795aaccf1c92902c478c2802fedda3a9c86e3
Timothy Klim <klimtimothy@gmail.com>
2015-02-06 21:01:16 +0300

    fix(deploy): sbt build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 export JAVA_OPTS="-Xms512M -Xmx2048M -Xss1M -XX:+CMSClassUnloadingEnabled"
-#sbt clean dist
+sbt clean dist
 # ansible-galaxy install -r ./ansible/requirements.yml
 echo "!!! For install requirements: ansible-galaxy install -r ./ansible/requirements.yml !!!"
 ansible-playbook -vvv -i ./ansible/hosts ./ansible/deploy.yml
