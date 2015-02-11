aa693187c51a3e014e8481e356531b2473484479
Andrey Kouznetsov <fear@loathing.in>
2015-02-11 20:18:25 +0300

    chore(*): don't start SMTP server

--- src/main/scala/im/actor/server/ApiKernel.scala
+++ src/main/scala/im/actor/server/ApiKernel.scala
@@ -9,7 +9,7 @@ import com.secretapp.backend.api._
 import com.secretapp.backend.api.frontend.tcp.TcpServer
 import com.secretapp.backend.api.frontend.ws.WSServer
 import com.secretapp.backend.session.SessionActor
-import im.actor.server.smtpd.{ SMTPServer, TLSActor }
+import im.actor.server.smtpd.TLSActor
 import com.typesafe.config._
 import java.net.InetSocketAddress
 import im.actor.server.persist.{ FlywayInit, DbInit }
@@ -47,7 +47,7 @@ class ApiKernel extends Bootable with FlywayInit with DbInit {
     val singletons = new Singletons
     val fileAdapter = new FileStorageAdapter(system)
 
-    val (keyManagerFactory, trustManagerFactory) = TLSActor.getManagerFactories() // check ssl configuration
+    //val (keyManagerFactory, trustManagerFactory) = TLSActor.getManagerFactories() // check ssl configuration
 
     val sessionReceiveTimeout = system.settings.config.getDuration("session.receive-timeout", MILLISECONDS)
     val sessionRegion = SessionActor.startRegion(singletons, fileAdapter, sessionReceiveTimeout.milliseconds)(system)
@@ -73,10 +73,10 @@ class ApiKernel extends Bootable with FlywayInit with DbInit {
     system.actorOf(Props(new WSHeatingUpActor(hostname, wsPort)), "ws-heat-service")
 
     // SMTP service
-    SMTPServer.start(singletons, keyManagerFactory, trustManagerFactory)
+    //SMTPServer.start(singletons, keyManagerFactory, trustManagerFactory)
 
     // REST api
-    HttpApiService.start(config, fileAdapter)
+    //HttpApiService.start(config, fileAdapter)
   }
 
   def shutdown() = {
