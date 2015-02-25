db01100071b79ca26255d510444b0a3c20308e16
Andrey Kouznetsov <fear@loathing.in>
2015-02-26 02:42:44 +0300

    fix(persist): fixed file_blocks migration

--- actor-persist/src/main/resources/sql/migration/V30__AddAdapterDataToFileBlocks.sql
+++ actor-persist/src/main/resources/sql/migration/V30__AddAdapterDataToFileBlocks.sql
@@ -1,1 +1,3 @@
-ALTER TABLE file_blocks ADD COLUMN adapter_data bytea NOT NULL;
+ALTER TABLE file_blocks ADD COLUMN adapter_data bytea;
+UPDATE file_blocks SET adapter_data = CAST('' as bytea);
+ALTER TABLE file_blocks ALTER COLUMN adapter_data SET NOT NULL;
