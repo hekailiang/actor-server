1b631bb64941c89161d09ffeb62aa024d817f27b
prettynatty <fear@loathing.in>
2015-04-10 16:05:34 +0300

    feat(auth): send activation codes through both clickatell and twilio

--- actor-api/src/main/scala/com/secretapp/backend/sms/SmsEngine.scala
+++ actor-api/src/main/scala/com/secretapp/backend/sms/SmsEngine.scala
@@ -3,7 +3,7 @@ package com.secretapp.backend.sms
 import scala.concurrent.Future
 
 trait SmsEngine {
-  protected def message(code: String) = s"Your actor activation code: $code"
+  protected def message(code: String) = s"Your Actor activation code: $code"
 
   def send(phoneNumber: Long, code: String): Future[String]
 }
